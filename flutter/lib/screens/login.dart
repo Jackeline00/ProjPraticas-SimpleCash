7cd8d43ed99116c3bf43953728e684f18245fec6
Jackeline00 <jackelinecristinaribeiro@gmail.com>
2025-09-22 11:34:23 -0300

    centralização e mudanças visuais na tela de login

--- flutter/lib/screens/login.dart
+++ flutter/lib/screens/login.dart
@@ -39,19 +39,31 @@ class _LoginScreen extends State<Login> {
   @override 
   Widget build(BuildContext context) { /// início da criação da tela
     return Scaffold( // layout base da tela
-      appBar: AppBar(title: const Text("Login")), // título principal
+      appBar: AppBar(
+        centerTitle: true, /// deixa o título centralizado
+        title: const Text(
+          "Login",
+          style: TextStyle( /// estilo do título Login
+            color: Color.fromARGB(255, 101, 144, 209),
+            fontSize: 30
+        ),
+      )), // título principal
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
+        child: Center(child: 
+          Form(
           key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
+              SizedBox(
+                width: 400,
+                child: TextFormField(
                 controller: _emailController, /// pega o valor digitado
                 decoration: const InputDecoration(
                   labelText: "E-mail",
                   border: OutlineInputBorder(),
+
                 ),
                 validator: (value) { /// valida o email
                   if (value == null || value.isEmpty) { /// verifica se ele foi digitado corretamente
@@ -59,9 +71,14 @@ class _LoginScreen extends State<Login> {
                   }
                   return null;
                 },
+                )
               ),
+              
               const SizedBox(height: 16),
-              TextFormField(
+              
+              SizedBox(
+                width: 400,
+                child: TextFormField(
                 controller: _senhaController, /// pega a senha digitada
                 decoration: const InputDecoration(
                   labelText: "Senha",
@@ -74,14 +91,21 @@ class _LoginScreen extends State<Login> {
                   }
                   return null;
                 },
+                )
               ),
-              const SizedBox(height: 24),
+
+              const SizedBox(height: 80),
               ElevatedButton(
                 onPressed: _login, /// botão chama a função _login ao ser precionado
-                child: const Text("Entrar"),
+                child: const Text(
+                  "Entrar",
+                  style: TextStyle(color: Color.fromARGB(255, 108, 153, 252)),
+                ),
+                  
               ),
             ],
           ),
+          )
         ),
       ),
     );
