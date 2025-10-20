4c4ba307d411cf07b49b039599687c4d6306c397
EstephanieAlves <cc23129@g.unicamp.br>
2025-10-20 08:53:52 -0300

    Mudando algumas coisas do monstrar poupança
    Updated mostrarPoupancas method to return a list of savings instead of a boolean. Adjusted response handling to decode JSON data.

--- flutter/lib/services/poupanca_service.dart
+++ flutter/lib/services/poupanca_service.dart
@@ -26,18 +26,28 @@ class PoupancaService {
     }
   }
 
-  Future<bool> mostrarPoupancas(int idUsuario) async{
-    final response = await http.get(
-      Uri.parse('$baseUrl/poupanca/$idUsuario'),
-      headers: {"Content-Type": "application/json"},
-    );
+  Future<List<Map<String, dynamic>>> mostrarPoupancas(int idUsuario) async {
+  final response = await http.get(
+    Uri.parse('$baseUrl/poupanca/$idUsuario'),
+    headers: {"Content-Type": "application/json"},
+  );
 
-     if(response.statusCode == 201){
-      return true;
-    }else{
-      return false;
-    }
+  if (response.statusCode == 200) {
+    final List<dynamic> data = jsonDecode(response.body);
+
+    return data.map((e) => {
+      "id": e["idPoupanca"],
+      "descricao": e["descricao"],
+      "valor": (e["valor"] as num).toDouble(),
+      "data": e["dataCriacao"],
+      "tipo": e["tipo"],
+      "origem": e["origem"],
+      "repeticao": e["repeticao"],
+    }).toList();
+  } else {
+    throw Exception('Erro ao carregar poupanças');
   }
+}
 
    Future<bool> editar(int id, String tipo, String descricao, double valor, String data, int repeticao, String origem) async{
     final response = await http.put(
